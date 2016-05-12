46ee0c8b577ce3034fe312b0a07007e88e4d6230
andrej fritz <a.fritz@betzold.de>
2016-05-12 09:52:44 +0200

    updated package

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'andruschka:autoform-selectize-methods',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: 'A Meteor-Methods based Selectize for Autoform',
+  summary: 'A Meteor-Methods based Selectize plugin for Autoform',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/andruschka/meteor-autoform-selectize-methods.git',
   // By default, Meteor will default to using README.md for documentation.
